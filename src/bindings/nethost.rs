cd43a63081a2cc492399f13716d4a0ed51c34546
OpenByte <development.openbyte@gmail.com>
2021-03-10 21:50:42 +0100

    Remove nethost dll related bindings

--- src/bindings/nethost.rs
+++ src/bindings/nethost.rs
@@ -1,7 +1,5 @@
 use std::{mem::size_of, ptr};
 
-use dlopen::wrapper::WrapperApi;
-
 use super::type_aliases::{char_t, size_t};
 
 // #[link(name = "nethost")]
@@ -35,39 +33,6 @@ extern "C" {
         buffer_size: *mut size_t,
         parameters: *const get_hostfxr_parameters,
     ) -> i32;
-}
-
-#[derive(WrapperApi)]
-pub struct NethostLib {
-    /// Get the path to the hostfxr library
-    ///
-    /// # Arguments
-    ///  * `buffer`:
-    ///     Buffer that will be populated with the hostfxr path, including a null terminator.
-    ///  * `buffer_size`:
-    ///     * \[in\] Size of buffer in [`char_t`] units.
-    ///     * \[out\] Size of buffer used in [`char_t`] units. If the input value is too small
-    ///           or `buffer` is [`null`](ptr::null()), this is populated with the minimum required size
-    ///           in [`char_t`] units for a buffer to hold the hostfxr path
-    ///
-    /// * `get_hostfxr_parameters`:
-    ///     Optional. Parameters that modify the behaviour for locating the hostfxr library.
-    ///     If [`null`](ptr::null()), hostfxr is located using the enviroment variable or global registration
-    ///
-    /// # Return value
-    ///  * 0 on success, otherwise failure
-    ///  * 0x80008098 - `buffer` is too small ([`HostApiBufferTooSmall`])
-    ///
-    /// # Remarks
-    /// The full search for the hostfxr library is done on every call. To minimize the need
-    /// to call this function multiple times, pass a large buffer (e.g. [`PATH_MAX`]).
-    ///
-    /// [HostApiBufferTooSmall](super::error::KnownHostExitCode::HostApiBufferTooSmall)
-    get_hostfxr_path: unsafe extern "C" fn(
-        buffer: *mut char_t,
-        buffer_size: *mut size_t,
-        parameters: *const get_hostfxr_parameters,
-    ) -> i32,
 }
 
 /// Parameters for `get_hostfxr_path`
