586e6de7e182e6d4064016c5862e433acf65818e
OpenByte <development.openbyte@gmail.com>
2021-07-05 21:30:05 +0200

    Remove nethost downloader

--- update-nethost.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-Powershell.exe -ExecutionPolicy RemoteSigned -File "update-nethost.ps1"
-pause
--- update-nethost.ps1
+++
@@ -1,1 +0,0 @@
-dotnet run --project "NethostDownloader\NethostDownloader.csproj" --framework "net6.0" --configuration "Release" -- --runtimes-directory "netcorehost\runtimes" --delete-old
