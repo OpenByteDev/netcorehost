6531047592adacee23eea8f26741a1279ff7c4ef
OpenByte <development.openbyte@gmail.com>
2021-09-02 22:30:39 +0200

    Fix doc link

--- src/bindings/nethost.rs
+++ src/bindings/nethost.rs
@@ -28,7 +28,7 @@ extern "system" {
     ///
     /// # Return value
     ///  * 0 on success, otherwise failure
-    ///  * 0x80008098 - `buffer` is too small ([`HostApiBufferTooSmall`](crate::hostfxr::HostingError::HostApiBufferTooSmall))
+    ///  * 0x80008098 - `buffer` is too small ([`HostApiBufferTooSmall`](crate::error::HostingError::HostApiBufferTooSmall))
     ///
     /// # Remarks
     /// The full search for the hostfxr library is done on every call. To minimize the need
