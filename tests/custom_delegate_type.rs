20d4e6730a13f429eb0fb1946ddbfd3b07c29128
OpenByte <development.openbyte@gmail.com>
2022-04-28 15:01:35 +0200

    Revert "Try to fix tests on CI"
    This reverts commit f6c757ef5ded266742f5fc1ee0980b275e67839a.

--- tests/custom_delegate_type.rs
+++ tests/custom_delegate_type.rs
@@ -1,33 +1,30 @@
 #![cfg(feature = "netcore3_0")]
 
 use netcorehost::{nethost, pdcstr};
-use rusty_fork::rusty_fork_test;
 
 #[path = "common.rs"]
 mod common;
 
-rusty_fork_test! {
-    #[test]
-    fn hello_world_with_custom_delegate_type() {
-        common::setup();
+#[test]
+fn hello_world_with_custom_delegate_type() {
+    common::setup();
 
-        let hostfxr = nethost::load_hostfxr().unwrap();
+    let hostfxr = nethost::load_hostfxr().unwrap();
 
-        let context = hostfxr
-            .initialize_for_runtime_config(pdcstr!(
-                "tests/Test/bin/Debug/net6.0/Test.runtimeconfig.json"
-            ))
-            .unwrap();
-        let fn_loader = context
-            .get_delegate_loader_for_assembly(pdcstr!("tests/Test/bin/Debug/net6.0/Test.dll"))
-            .unwrap();
-        let hello = fn_loader
-            .get_function::<fn()>(
-                pdcstr!("Test.Program, Test"),
-                pdcstr!("CustomHello"),
-                pdcstr!("Test.Program+CustomHelloFunc, Test"),
-            )
-            .unwrap();
-        hello();
-    }
+    let context = hostfxr
+        .initialize_for_runtime_config(pdcstr!(
+            "tests/Test/bin/Debug/net6.0/Test.runtimeconfig.json"
+        ))
+        .unwrap();
+    let fn_loader = context
+        .get_delegate_loader_for_assembly(pdcstr!("tests/Test/bin/Debug/net6.0/Test.dll"))
+        .unwrap();
+    let hello = fn_loader
+        .get_function::<fn()>(
+            pdcstr!("Test.Program, Test"),
+            pdcstr!("CustomHello"),
+            pdcstr!("Test.Program+CustomHelloFunc, Test"),
+        )
+        .unwrap();
+    hello();
 }
