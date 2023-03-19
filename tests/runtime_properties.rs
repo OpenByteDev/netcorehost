8e3e5712feef1cd1ad3818e78f4aaa8b8f823e66
OpenByte <development.openbyte@gmail.com>
2023-03-19 14:30:48 +0100

    Fix clippy warnings

--- tests/runtime_properties.rs
+++ tests/runtime_properties.rs
@@ -19,10 +19,10 @@ rusty_fork_test! {
         let test_property_name = pdcstr!("TEST_PROPERTY");
         let test_property_value = pdcstr!("TEST_VALUE");
         context
-            .set_runtime_property_value(&test_property_name, &test_property_value)
+            .set_runtime_property_value(test_property_name, test_property_value)
             .unwrap();
         let property_value = context
-            .get_runtime_property_value(&test_property_name)
+            .get_runtime_property_value(test_property_name)
             .unwrap();
         assert_eq!(test_property_value, property_value.as_ref());
 
