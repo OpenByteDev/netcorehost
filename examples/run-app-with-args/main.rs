2868dcbd1334cf6a2c230b2546b0a5e55684ac30
OpenByte <development.openbyte@gmail.com>
2023-08-11 16:42:21 +0200

    Cargo fmt

--- examples/run-app-with-args/main.rs
+++ examples/run-app-with-args/main.rs
@@ -1,9 +1,6 @@
 use std::{env, iter};
 
-use netcorehost::{
-    nethost,
-    pdcstring::PdCString, pdcstr,
-};
+use netcorehost::{nethost, pdcstr, pdcstring::PdCString};
 
 fn main() {
     let hostfxr = nethost::load_hostfxr().unwrap();
@@ -13,14 +10,16 @@ fn main() {
         .map(|arg| PdCString::from_os_str(arg).unwrap())
         .collect::<Vec<_>>();
     let borrowed_args = program_args.iter().map(|arg| arg.as_ref());
-    let app_and_args = iter::once(pdcstr!("examples/run-app-with-args/ExampleProject/bin/Debug/net6.0/ExampleProject.dll"))
-        .chain(borrowed_args)
-        .collect::<Vec<_>>();
+    let app_and_args = iter::once(pdcstr!(
+        "examples/run-app-with-args/ExampleProject/bin/Debug/net6.0/ExampleProject.dll"
+    ))
+    .chain(borrowed_args)
+    .collect::<Vec<_>>();
 
     let context = hostfxr
         .initialize_for_dotnet_command_line_with_args(&app_and_args)
         .unwrap();
-    
+
     let result = context.run_app().value();
     println!("Exit code: {}", result);
 }
