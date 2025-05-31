7bef883b28bbac5ec97c46ca46dfb10e463721c0
OpenByte <development.openbyte@gmail.com>
2025-05-31 15:22:22 +0200

    dotnet build ignores --framework

--- tests/common.rs
+++ tests/common.rs
@@ -13,10 +13,22 @@ pub fn test_netcore_version() -> String {
     env::var("NETCOREHOST_TEST_NETCORE_VERSION").unwrap_or_else(|_| "net8.0".to_string())
 }
 
+pub fn test_project_file_path() -> PathBuf {
+    PathBuf::from_str(&format!(
+        "tests/Test/Test-{}.csproj",
+        test_netcore_version()
+    ))
+    .unwrap()
+    .absolutize()
+    .unwrap()
+    .to_path_buf()
+}
+
 pub fn test_runtime_config_path() -> PdCString {
     PdCString::from_os_str(
         PathBuf::from_str(&format!(
-            "tests/Test/bin/Debug/{}/Test.runtimeconfig.json",
+            "tests/Test/bin/Debug/{}/Test-{}.runtimeconfig.json",
+            test_netcore_version(),
             test_netcore_version()
         ))
         .unwrap()
@@ -30,7 +42,34 @@ pub fn test_runtime_config_path() -> PdCString {
 pub fn test_dll_path() -> PdCString {
     PdCString::from_os_str(
         PathBuf::from_str(&format!(
-            "tests/Test/bin/Debug/{}/Test.dll",
+            "tests/Test/bin/Debug/{}/Test-{}.dll",
+            test_netcore_version(),
+            test_netcore_version()
+        ))
+        .unwrap()
+        .absolutize()
+        .unwrap()
+        .as_os_str(),
+    )
+    .unwrap()
+}
+
+pub fn library_project_file_path() -> PathBuf {
+    PathBuf::from_str(&format!(
+            "tests/ClassLibrary/ClassLibrary-{}.csproj",
+        test_netcore_version()
+    ))
+    .unwrap()
+    .absolutize()
+    .unwrap()
+    .to_path_buf()
+}
+
+pub fn library_symbols_path() -> PdCString {
+    PdCString::from_os_str(
+        PathBuf::from_str(&format!(
+            "tests/ClassLibrary/bin/Debug/{}/ClassLibrary-{}.pdb",
+            test_netcore_version(),
             test_netcore_version()
         ))
         .unwrap()
@@ -44,21 +83,8 @@ pub fn test_dll_path() -> PdCString {
 pub fn library_dll_path() -> PdCString {
     PdCString::from_os_str(
         PathBuf::from_str(&format!(
-            "tests/ClassLibrary/bin/Debug/{}/ClassLibrary.dll",
-            test_netcore_version()
-        ))
-        .unwrap()
-        .absolutize()
-        .unwrap()
-        .as_os_str(),
-    )
-    .unwrap()
-}
-
-pub fn library_symbols_path() -> PdCString {
-    PdCString::from_os_str(
-        PathBuf::from_str(&format!(
-            "tests/ClassLibrary/bin/Debug/{}/ClassLibrary.pdb",
+            "tests/ClassLibrary/bin/Debug/{}/ClassLibrary-{}.dll",
+            test_netcore_version(),
             test_netcore_version()
         ))
         .unwrap()
@@ -79,12 +105,16 @@ pub fn build_test_project() {
         return;
     }
 
+    let netcore_version = test_netcore_version();
+    let project_file_path = test_project_file_path();
+    let project_dir = project_file_path.parent().unwrap();
+
     Command::new("dotnet")
         .arg("build")
-        .arg("Test.sln")
+        .arg(&project_file_path)
         .arg("--framework")
-        .arg(&test_netcore_version())
-        .current_dir("tests/Test")
+        .arg(netcore_version)
+        .current_dir(project_dir)
         .spawn()
         .expect("dotnet build failed")
         .wait()
@@ -96,12 +126,16 @@ pub fn build_library_project() {
         return;
     }
 
+    let netcore_version = test_netcore_version();
+    let project_file_path = library_project_file_path();
+    let project_dir = project_file_path.parent().unwrap();
+
     Command::new("dotnet")
         .arg("build")
-        .arg("ClassLibrary.sln")
+        .arg(&project_file_path)
         .arg("--framework")
-        .arg(&test_netcore_version())
-        .current_dir("tests/ClassLibrary")
+        .arg(netcore_version)
+        .current_dir(project_dir)
         .spawn()
         .expect("dotnet build failed")
         .wait()
